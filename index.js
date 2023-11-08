537c8707e577ae70447e406e84e42365fca730d3
Sam Foxman <elasticvine@protonmail.com>
2023-11-08 14:43:09 -0800

    table

--- index.js
+++ index.js
@@ -1,34 +1,80 @@
 (function() {
-    const date = new Date();
-    const dayOfWeek = ['U', 'M', 'T', 'W', 'R', 'F', 'S'][date.getDay()];
-    const now = date.getHours() + 1/60 * date.getMinutes();
+    function td(text) {
+        const elem = document.createElement('td');
+        elem.innerText = text;
+        return elem;
+    }
 
-    for (let course of FA2023) {
-        let foundSection;
-        let foundTimeId;
-        for (let section of course.sections) {
-            for (let [i, time] of section.times.split("\n").entries()) {
-                if (time.includes(dayOfWeek)) {
-                    const parts = time.split(' ');
-                    const [startHour, startMin] = parts[1].split(':').map(x => parseInt(x));
-                    const start = startHour + 1/60*startMin;
-                    const [endHour, endMin] = parts[3].split(':').map(x => parseInt(x));
-                    const end = endHour + 1/60*endMin;
-                    if (start <= now && end >= now) {
-                        foundSection = section;
-                        foundTimeId = i;
-                        break;
+    function displayCourse({course, foundSection, foundTimeId}) {
+        const elem = document.createElement('tr');
+        elem.appendChild(td(course.number));
+        elem.appendChild(td(course.name));
+        elem.appendChild(td(foundSection.locations.split('\n')[foundTimeId]));
+        elem.appendChild(td(foundSection.times.split('\n')[foundTimeId]));
+        document.getElementsByTagName('tbody')[0].appendChild(elem);
+    }
+
+    function findCourses(date) {
+        const out = [];
+
+        const dayOfWeek = ['U', 'M', 'T', 'W', 'R', 'F', 'S'][date.getDay()];
+        const now = date.getHours() + 1/60 * date.getMinutes();
+
+        for (let course of FA2023) {
+            if (course.found) {
+                continue;
+            }
+
+            let foundSection;
+            let foundTimeId;
+            for (let section of course.sections) {
+                const locations = section.locations.split('\n');
+                for (let [i, time] of section.times.split("\n").entries()) {
+                    if (!time.startsWith('OM') && time.includes(dayOfWeek) && locations[i] !== 'A') {
+                        const parts = time.split(' ');
+                        const [startHour, startMin] = parts[1].split(':').map(x => parseInt(x));
+                        const start = startHour + 1/60*startMin;
+                        const [endHour, endMin] = parts[3].split(':').map(x => parseInt(x));
+                        const end = endHour + 1/60*endMin;
+                        if (start <= now && end >= now) {
+                            foundSection = section;
+                            foundTimeId = i;
+                            break;
+                        }
                     }
+                }
+                if (foundSection) {
+                    break;
                 }
             }
             if (foundSection) {
-                break;
+                course.found = true;
+                out.push({
+                    course,
+                    foundSection,
+                    foundTimeId
+                });
             }
         }
-        if (foundSection) {
-            const elem = document.createElement('p');
-            elem.innerText = course.name + ' | ' + foundSection.times.split('\n')[foundTimeId] + ' ' + foundSection.locations.split('\n')[foundTimeId];
-            document.body.appendChild(elem);
+        return out;
+    }
+
+    for (let info of findCourses(new Date())) {
+        displayCourse(info);
+    }
+
+    const upcoming = new Date();
+    upcoming.setMinutes(60); // end of this hour
+    const upcomingInfos = findCourses(upcoming);
+    if (upcomingInfos.length) {
+        const splitter = document.createElement('tr');
+        splitter.classList.add('splitter');
+        for (let i = 0; i < 4; i++) {
+            splitter.appendChild(td('_'));
+        }
+        document.getElementsByTagName('tbody')[0].appendChild(splitter);
+        for (let info of upcomingInfos) {
+            displayCourse(info);
         }
     }
 })();
